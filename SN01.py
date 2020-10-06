bcca0524908dff6f6de854b4799d45537558f705
☒ Bjarke Gotfredsen <gotfredsen@users.noreply.github.com>
2020-10-06 17:30:41 +0200

    Add Almanac download

--- SN01.py
+++ SN01.py
@@ -1,14 +1,26 @@
 # First: sudo pigpiod
+# Python3!
 
 from time import sleep
+import requests
 import pigpio
 import pynmea2
+
+# Get a token here: http://www.u-blox.com/services-form.html
+token="insert_token_here"
 
 pi = pigpio.pi()
 addr    = 0x42
 bus     = 1
 getlen  = 0xFD
 getdata = 0xFF
+
+def getAlamanac():
+    url=f'http://online-live1.services.u-blox.com/GetOnlineData.ashx?token={token};gnss=gps;datatype=eph,alm,aux,pos;filteronpos;format=aid'
+    r = requests.get(url)
+    handle = pi.i2c_open(bus, addr)
+    pi.i2c_write_device(handle, r.content)
+    pi.i2c_close(handle)
 
 def SN01_data():
     rl =0
@@ -24,7 +36,7 @@ def SN01_data():
         return data.decode(encoding='UTF-8',errors='ignore').splitlines()
     return None
 
-while(True):
+def loop():
     data = SN01_data()
     if data:
         for t in data:
@@ -34,3 +46,10 @@ while(True):
             except:
                 pass
     sleep(0.1)
+
+getAlamanac()
+print("Almanac downloaded")
+
+while(True):
+    loop()
+
